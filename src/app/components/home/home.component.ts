5fbdedc70759cb8be4d3cd4e80611eb24bb5d6fa
lboase023 <lboase023@student.wethinkcode.co.za>
2026-05-15 10:49:12 +0200

    added our leaders section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  leaders = [
+    {
+      image: 'assets/leader-1.jpeg',
+      name: 'Pastor Riaan & Mirjam Botha',
+      title: 'Lead Pastor',
+      description:
+        'Pastor Riaan & Mirjam Botha provide spiritual direction for the church, lead the teaching ministry, and care for the overall growth of the congregation.'
+    },
+    {
+      image: 'assets/leader-2.jpeg',
+      name: 'Aaron & Lusanda Matentshe',
+      title: 'Outreach Directors',
+      description:
+      'Aaron & Lusanda Matentshe lead outreach efforts, connect the church with the local community, and oversee the church operations.'
+        
+    },
+    {
+      image: 'assets/leader-3.jpeg',
+      name: 'Pieter & Safiya Mahlangu',
+      title: 'Evangelist Pieter ',
+      description:
+      'Evangelist Pieter supports '
+    }
+  ];
 }
