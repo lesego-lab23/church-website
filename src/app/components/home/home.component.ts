a4c3b7febe0256aef2c47036dcfcbb7190acea8d
lboase023 <lboase023@student.wethinkcode.co.za>
2026-05-22 15:36:06 +0200

    home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,25 +9,15 @@ export class HomeComponent {
   leaders = [
     {
       image: 'assets/leader-1.jpeg',
-      name: 'Pastor Riaan & Mirjam Botha',
-      title: 'Lead Pastor',
-      description:
-        'Pastor Riaan & Mirjam Botha provide spiritual direction for the church, lead the teaching ministry, and care for the overall growth of the congregation.'
+      name: 'Pastor Riaan & Mirjam Botha'
     },
     {
       image: 'assets/leader-2.jpeg',
-      name: 'Aaron & Lusanda Matentshe',
-      title: 'Outreach Directors',
-      description:
-      'Aaron & Lusanda Matentshe lead outreach efforts, connect the church with the local community, and oversee the church operations.'
-        
+      name: 'Aaron & Lusanda Matentshe'
     },
     {
       image: 'assets/leader-3.jpeg',
-      name: 'Pieter & Safiya Mahlangu',
-      title: 'Evangelist Pieter ',
-      description:
-      'Evangelist Pieter supports '
+      name: 'Pieter & Safiya Mahlangu'
     }
   ];
 }
